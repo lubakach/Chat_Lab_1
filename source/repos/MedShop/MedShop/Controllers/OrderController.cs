56ad0a2c5a2fd70182ce490b90c886762164b749
lubakach <lubakach26@gmail.com>
2020-12-07 02:05:20 +0300

    update

--- source/repos/MedShop/MedShop/Controllers/OrderController.cs
+++ source/repos/MedShop/MedShop/Controllers/OrderController.cs
@@ -25,29 +25,35 @@ namespace MedShop.Controllers
         }
 
         public IActionResult Checkout() {
-            return View();
-            if (User.Identity.IsAuthenticated)
-            {
-                return View();
+            shopCart.listShopItems = shopCart.getShopItems();
+            
+            if (shopCart.listShopItems.Count == 0) {
+                return RedirectToAction("Error");
             }
-            else return RedirectToAction("AuthoError");
+            if (shopCart.listShopItems.Count == 0 & User.Identity.IsAuthenticated!)
+            {
+                return RedirectToAction("AuthoError");
+            }
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("AuthoError");
+            }
+            else return View();
         }
         [HttpPost]
         public IActionResult Checkout(Order order)
         {
-            
-                shopCart.listShopItems = shopCart.getShopItems();
-                if (ModelState.IsValid)
-                {
-                    allOrders.createOrder(order);
-                    service.SendEmailDefault();
-                    return RedirectToAction("Complete");
-                }
-                else
+            shopCart.listShopItems = shopCart.getShopItems();
+            if (shopCart.listShopItems.Count == 0)
+            {
                 return RedirectToAction("Error");
-            
-            
-
+            }
+            if (ModelState.IsValid)
+            {
+                allOrders.createOrder(order);
+                return RedirectToAction("Complete");
+            }
+            return View(order);
         }
         public IActionResult Error()
         {
